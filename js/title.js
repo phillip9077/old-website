10a260de68894db370841fd679d576b3104972d6
Phillip Wei <wei.ph@husky.neu.edu>
2020-05-18 14:34:46 +0800

    Updated intro section with animation and hamburger menu

---
+++ js/title.js
@@ -0,0 +1,32 @@
+const text = document.querySelector('.title');
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+for(let i = 0; i < splitText.length; i++) {
+    let char = "";
+    if(splitText[i] === " ") {
+        char = splitText[i] + "&nbsp";
+    }
+    else {
+        char = splitText[i];
+    }
+    text.innerHTML += "<span>" + char + "</span>";
+}
+ 
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    if(char === splitText.length) {
+        complete();
+        return; 
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
